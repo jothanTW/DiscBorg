abb3555dac61a1b0aa4d715b1b5effdc4e8afd20
J <asdfg_john@yahoo.com>
2023-07-16 15:33:20 -0500

    Migrated project to newest .Net; removed dependencies from repo

---
+++ RefBot/RefBot/IBorg.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordDSPTestConnect
+{
+    interface IBorg
+    {
+        string reply(string input);
+        string learn(string input);
+        string command(string input, bool isAdmin);
+        string loadMem(string filename);
+        string saveMem(string filename);
+        string getHelp(string inp);
+    }
+}
